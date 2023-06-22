e1a45ef40609432a839b47d84d7c85cde17a57af
Renata Guerra <re.guerr@gmail.com>
2023-06-21 21:14:53 -0300

    Added date format

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 import './global.css';
 
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/reguerr.png',
+      name: 'Renata Guerra',
+      role: 'CTO @Alegrim'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return' },
+      { type: 'link', content: 'diego.design/doctorcare' },  
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/reguerr.png',
+      name: 'Renata Guerra',
+      role: 'CTO @ Alegrim'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return' },
+      { type: 'link', content: 'diego.design/doctorcare' },  
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -13,7 +44,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {post.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
